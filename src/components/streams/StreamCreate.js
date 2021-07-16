3f5c84ac35682794562e6878483f51810de7de06
Slim <sazwerling@gmail.com>
2021-07-16 12:52:06 -0500

    highlighting errors

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -2,13 +2,26 @@ import React from "react";
 import { Field, reduxForm } from 'redux-form';
 
 class StreamCreate extends React.Component {
-    renderInput({ input, label, meta }){     //when called this function gets passed - as argt - a bunch of stuff like 'value' and 'onChange'
-        return (                            //takes all input props from formProps.input object and adds to input element
-                                            //props 'label' gets passed into formProps as well so can destructure as 'label'
-           <div className="field">       
+    renderError({ error, touched }){   //takes in meta and destructures error and touched
+        if(touched && error){
+            return(
+                <div className="ui error message">
+                    <div className="header">
+                        {error}
+                    </div>
+                </div>
+            );
+        }
+    }
+
+    renderInput = ({ input, label, meta }) => {    //when called this function gets passed - as argt - a bunch of stuff like 'value' and 'onChange'
+                                                    //takes all input props from formProps.input object and adds to input element, //props 'label' gets passed into formProps as well so can destructure as 'label'
+        const className = `field ${meta.error && meta.touched ? 'error' : ''}` //ternary to determine classname with error or not
+        return (                                                                  
+           <div className={className}>       
                 <label>{label}</label>
-                <input {...input}/>  
-                <div>{meta.error}</div> 
+                <input {...input} autoComplete="off"/>  
+                {this.renderError(meta)} 
             </div>
         )                          
     }  
@@ -18,10 +31,14 @@ class StreamCreate extends React.Component {
     onSubmit(formValues){
         console.log(formValues)
     }
-
+//if a field has same name as proptery in object returned from 'validate' function, it gets passed to renderInput
+//and is accessed as meta.error
    render() {
       return (
-          <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form">
+          <form 
+          onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form"
+          className="ui form error" //needs this className or semantic ui blocks errors from appearing
+          >
               <Field name="title" component={this.renderInput} label="Enter Title"/> 
               <Field name="description" component={this.renderInput} label="Enter Description"/>
               <button className="ui button primary">Submit</button>
