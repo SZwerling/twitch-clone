90753013d656fd47f12c656f253f9b09fdc7bf17
Slim <sazwerling@gmail.com>
2021-07-23 10:26:08 -0500

    programmatic nav

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,6 @@
 import { SIGN_IN, SIGN_OUT, CREATE_STREAM, FETCH_STREAMS, FETCH_STREAM, DELETE_STREAM, EDIT_STREAM } from "./types";
-import streams from '../apis/streams';    
+import streams from '../apis/streams';  
+import history from '../history';  
 
 export const signIn = (userId) => {
     return {
@@ -21,7 +22,7 @@ export const createStream = (formValues) => async (dispatch, getState) => {
     const response = await streams.post('/streams', {...formValues, userId});
 
     dispatch({ type: CREATE_STREAM, payload: response.data })
-    //programmatic navigation
+    history.push('/');
 };
 
 export const fetchStreams = () => async dispatch => {
